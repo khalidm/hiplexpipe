73957978fdb4c9ff5cc0f808f2a597ed104974da
Khalid Mahmood <mahmood.khalid@gmail.com>
2017-03-14 10:58:46 +1100

    update stages bcftools and undr_rover

--- src/pipeline.py
+++ src/pipeline.py
@@ -95,9 +95,9 @@ def make_pipeline(state):
         name='apply_samtools_mpileup',
         input=output_from('sort_bam_picard'),
         #filter=suffix('.sort.bam'),
+        ilter=formatter('.+/(?P<sample>[a-zA-Z0-9-]+).sort.bam'),
         output='all.bcf')
         #filter=formatter('.+/(?P<sample>[a-zA-Z0-9-]+).sort.bam'),
-        filter=formatter('.+/(?P<sample>[a-zA-Z0-9-]+).sort.bam'),
         #output='variants/all.bcf')
         .follows('sort_bam_picard'))
 
