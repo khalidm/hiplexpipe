fd3a08de31cc317a20965065cad688f9f36c2ac9
Khalid Mahmood <mahmood.khalid@gmail.com>
2017-04-04 14:01:41 +1000

    fix vep bug

--- src/stages.py
+++ src/stages.py
@@ -195,14 +195,14 @@ class Stages(object):
         '''Apply VEP'''
         vcf_in = inputs
         cores = self.get_stage_options('apply_vep', 'cores')
-        vep_command = "{vep_path}/variant_effect_predictor.pl --cache -i {vcf_in} --format vcf -o {vcf_vep} --force_overwrite --vcf " \
-                    "--sift b --polyphen b --symbol --numbers --biotype --total_length --offline --fields " \
-                    "Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE".format(
-                    vep_path=self.vep_path, vcf_in=vcf_in, vcf_vep=vcf_out)
+        vep_command = "{vep_path}/variant_effect_predictor.pl --cache --refseq --offline --fasta {reference} " \
+                    "-i {vcf_in} --sift b --polyphen b --symbol --numbers --biotype --total_length --hgvs " \
+                    "--format vcf -o {vcf_vep} --force_overwrite --vcf " \
+                    "--fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT," \
+                    "Protein_position,BIOTYPE,HGVSc,HGVSp,cDNA_position,CDS_position,HGVSc,HGVSp,cDNA_position,CDS_position " \
+                    "--fork {threads}".format(
+                    reference=self.reference, vep_path=self.vep_path, vcf_in=vcf_in, vcf_vep=vcf_out, threads=cores)
         run_stage(self.state, 'apply_vep', vep_command)
-        #--cache -i VTVCF --cache --sift b --polyphen b --symbol --numbers --biotype --total_length
-        #--force_overwrite --fork THREADS -o VEPVCF --vcf --offline --fields Consequence,Codons,Amino_acids,Gene,
-        #SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE
 
     def apply_bcf(self, inputs, vcf_out):
         '''Apply BCF'''
