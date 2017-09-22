20d03067a0ad53866328f911e8a067d5c5609273
Khalid Mahmood <mahmood.khalid@gmail.com>
2017-09-22 10:09:42 +1000

    add interval file

--- src/stages.py
+++ src/stages.py
@@ -136,7 +136,7 @@ class Stages(object):
         '''Clip the BAM file using Bamclipper'''
         bamclipper_args = '{bamclipper} -b {bam_in} -p {primer_bedpe_file} -n 1'.format(
                           bamclipper=self.bamclipper, bam_in=bam_in, primer_bedpe_file=self.primer_bedpe_file)
-        run_stage('clip_bam', bamclipper_args)
+        run_stage(self.state, 'clip_bam', bamclipper_args)
 
     def sort_bam_picard(self, bam_in, sorted_bam_out):
         '''Sort the BAM file using Picard'''
